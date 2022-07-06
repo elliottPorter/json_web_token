acb01e6f52ff9293ea25f175c5937be8c2fae512
Elliott Porter <elliott@weblake.co.uk>
2022-07-06 21:05:13 +0100

    parsing body of POST request

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 // require express
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 8888
+const app = express();
 
-app.get('/status', ( req, res) => {
-    const localTime = (new Date()).toLocaleTimeString()
+const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+	const user = req.body.username;
 
     res
-    .status(200)
-    .send(`Server time is ${localTime}`)
-})
+    .status(200).send(`You have logged in with ${user}`)
+});
 
-app.get('*', ( req, res ) => {
+app.get('/status', (req, res) => {
+	const localTime = new Date().toLocaleTimeString();
 
-    res
-    .status(404)
-    .send(`Page cannot be found`);
-})
+	res.status(200).send(`Server time is ${localTime}`);
+});
+
+app.get('*', (req, res) => {
+	res.status(404).send(`Page cannot be found`);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+	console.log(`Server is listening on port ${PORT}`);
+});
