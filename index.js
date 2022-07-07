2287d12023d4faca5fc7d50d348e110dc611857b
Elliott Porter <elliott@weblake.co.uk>
2022-07-07 08:26:29 +0100

    json web token successful

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 // require express
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 const PORT = process.env.PORT || 8888;
+
+const users = [
+	{ id: 1, username: 'admin', password: 'admin' },
+	{ id: 2, username: 'guest', password: 'guest' },
+];
 
 app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
-	const user = req.body.username;
+	if (!req.body.username || !req.body.password) {
+		res.status(400).send('A username and password is required');
+		return;
+	}
 
-    res
-    .status(200).send(`You have logged in with ${user}`)
+	const user = users.find((u) => {
+		return u.username === req.body.username && u.password === req.body.password;
+	});
+
+	if (!user) {
+		res.status(401).send('User not found');
+		return;
+	}
+
+	const token = jwt.sign(
+		{
+			sub: user.id,
+			username: user.username,
+		},
+		'mysecrettoken',
+		{ expiresIn: '3 hours' }
+	);
+
+	res.status(200).send({ access_token: token });
 });
 
 app.get('/status', (req, res) => {
